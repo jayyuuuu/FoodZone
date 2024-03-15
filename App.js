150e025b2e044778692e4bfca73f4f6c0a9ed61c
jayyuuuu <mca10051.19@bitmesra.ac.in>
2024-03-16 01:10:50 +0530

    E4 Day4

--- App.js
+++ App.js
@@ -1,31 +1,91 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React.createElement => React Element -JS Object => (render)HTML Element.
-// JSX - HTML like or XML like Syntex // Not HTML in JS.
-// JSX (transpiled before it reaches to JS)-  PARCEL - Babel
-// JSX => Babel transpiles it to React.CreateElement => React Element- JS object => (render) HTML Element.
+/**
+ * Components Our App Can have..
+ *
+ * Header
+ * - Logo
+ * - NavItems
+ * Body
+ * - search
+ * - RestorantContainer
+ * - Restorent Cards
+ * Footer
+ * - Copyright
+ * - Links
+ * - Address
+ * - Contacts
+ */
 
-//react element
-// const heading = <h1 className="head">React from JSX....</h1>;
-
-const title = (
-  <h1 className="head" tabIndex="5">
-    React from JSX....
-  </h1>
-);
-
-const HeadingComponent = () => {
+//Header
+const Header = () => {
   return (
-    <div id="container">
-      {title}
-      <h1 className="heading"> I am React From React Functional Component</h1>
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://images-platform.99static.com/dp8ehTneJPeFUiGc7OQFBtqSKrU=/460x460:1522x1522/500x500/top/smart/99designs-contests-attachments/132/132147/attachment_132147277"
+        ></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+//Res- Card Component
+const RestorentCard = () => {
+  return (
+    <div className="res-card">
+      <img
+        className="res-img"
+        src="https://plus.unsplash.com/premium_photo-1674106347866-8282d8c19f84?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      ></img>
+      <h3> Makkhan Da Dhaba</h3>
+      <h4>Dal Bati, Rajasthani, Indori</h4>
+      <h5>4.9 stars</h5>
+      <h6>20 minutes</h6>
     </div>
   );
 };
 
-// console.log(heading);
+//Body Component
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">
+        <input className="input" type="text"></input>
+        <button className="search-Btn">Search</button>
+      </div>
+      <div className="res-container">
+        <RestorentCard />
+        <RestorentCard />
+        <RestorentCard />
+        <RestorentCard />
+        <RestorentCard />
+        <RestorentCard />
+      </div>
+    </div>
+  );
+};
+
+//Top Level Component
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
